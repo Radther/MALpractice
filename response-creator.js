2aebeda9d49f9c1c12e9729a9bc3e64b7653454d
Tom Singleton <tomquil13@gmail.com>
2016-10-17 15:31:10 +0100

    add response created to create responses
    Created a module to create responses with a message and data.

---
+++ response-creator.js
@@ -0,0 +1,16 @@
+'use strict'
+
+exports.createResponse = function(message, data) {
+	return {
+		status: "Success",
+		message: message,
+		data: data
+	}
+}
+
+exports.createError = function(message) {
+	return {
+		status: "Error",
+		message: message
+	}
+}
