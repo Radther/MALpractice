24441b8e5dc17bc687a9a9aa56473441793d4903
Tom Singleton <tomquil13@gmail.com>
2016-11-07 14:34:06 +0000

    Couple updates

--- scratchpad.js
+++ scratchpad.js
@@ -6,41 +6,23 @@
 const request = require('request-promise')
 const xml2js = require('xml2js-es6-promise')
 const malintent = require('./malintent.js')
+const storage = require('node-persist')
 require('./extensions.js')
 
 const username = 'unistudent'
 const password = '+)}/wnP.G46D63TkUKq4'
 const fakePassword = 'notARealPassword'
 
-// malintent.verifyUserPromise(username, fakePassword)
-// 	.then( data => {
-// 		data.print()
-// 	}).catch(err => {
-// 		err.print()
-// 	})
-
-function testPromise() {
-	return new Promise(function(resolve, reject) {
-		request.get({
-			url: 'https://myanimelist.net/api/anime/search.xml?q=fate',
-			headers: {
-				'Authorization': 'Basic cmFkdGhlcjo0Mi5ZV3JBby5hMj1lVg=='
-			},
-			simple: false,
-			resolveWithFullResponse: true
-		})
-		.then((body)  => {
-			resolve(body)
-		}).catch( err => {
-			reject(err)
-		})
-	})
-}
-
-testPromise().then( data => {
-	data.print()
-}).catch( err => {
-	err.print()
+storage.initSync({
+	dir: 'data/anime',
+	stringify: JSON.stringify,
+	parse: JSON.parse,
+	encoding: 'utf8'
 })
 
+storage.getItemSync('1').print()
 
+// storage.setItem('1', {
+// 	title: 'Cowboy',
+// 	episodes: '12'
+// })
